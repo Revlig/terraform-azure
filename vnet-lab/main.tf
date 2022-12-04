27d899604f4d819912290ed518b75b7a20e11671
Gilver <gilver.souza@cellereit.com.br>
2022-12-04 00:20:35 -0300

    Desculpe a obra, estamos em transtornos

---
+++ vnet-lab/main.tf
@@ -0,0 +1,18 @@
+resource "random_id" "rg_name" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "example" {
+  location = "East US"
+  name     = "test-${random_id.rg_name.hex}-rg"
+}
+
+module "vnet" {
+  source              = "../vnet-module"
+  resource_group_name = azurerm_resource_group.example.name
+  vnet_location       = "East US"
+}
+
+provider "azurerm" {
+  features {}
+}
